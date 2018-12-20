13e87e1681b4e6cdcde416e3216696f50c83feb0
Valter Barros <valterinside@gmail.com>
2018-12-20 17:08:06 -0300

    master
    - Add tabbbar and tab-item component
    - Add handle for change is-selected class when user click on tab-item
    
    Signed-off-by: Valter Barros <valterinside@gmail.com>

--- frontend/packs/application.js
+++ frontend/packs/application.js
@@ -4,6 +4,12 @@ import router from '../router'
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css';
 import "../init/shared.css"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTasks, faMapMarkerAlt, faUser, faListUl, faCalendar, faFileAlt, faBox, faUserTie } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTasks, faMapMarkerAlt, faUser, faListUl, faCalendar, faFileAlt, faBox, faUserTie)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(MintUI)
 
--- frontend/pages/layouts/app.vue
+++ frontend/pages/layouts/app.vue
@@ -6,7 +6,36 @@
       </div>
       <router-link v-if="show_button(['order-index'])" :to="{name: 'order-new'}" slot="right">Novo Pedido</router-link>
     </mt-header>
+
     <router-view></router-view>
+    
+    <mt-tabbar class="position-fixed" :fixed="true">
+      <a class="mint-tab-item" id="tab1" 
+        :class="{ 'is-selected': selectedTabItem === 'tab1' }" 
+        @click="handle_tab_item_click($event, 'orders')">
+        
+        <div class="mint-tab-item-icon">
+          <font-awesome-icon icon="box" size="lg"></font-awesome-icon>
+        </div> 
+        
+        <div class="mint-tab-item-label">
+          Pedidos
+        </div>
+      </a>
+
+      <a class="mint-tab-item" id="tab2"
+        :class="{ 'is-selected': selectedTabItem === 'tab2' }"
+        @click="handle_tab_item_click($event, 'boss')">
+
+        <div class="mint-tab-item-icon">
+          <font-awesome-icon icon="user-tie" size="lg"></font-awesome-icon>
+        </div> 
+        
+        <div class="mint-tab-item-label">
+          Patrão
+        </div>
+      </a>
+    </mt-tabbar>
   </div>
 </template>
 
@@ -14,6 +43,27 @@
 import showButtonMixin from '../mixins/show_button_mixin.js'
 
 export default {
-  mixins: [showButtonMixin]
+  mixins: [showButtonMixin],
+  data() {
+    return {
+      selectedTabItem: '',
+      currentRoute: ''
+    }
+  },
+  mounted() {
+    this.currentRoute = this.$router.currentRoute.name
+
+    if(this.currentRoute === 'order-index') {
+      this.selectedTabItem = 'tab1'
+    } else if(this.currentRoute === 'boss'){
+      this.selectedTabItem = 'tab2'
+    }
+  },
+  methods: {
+    handle_tab_item_click: function(event, router_link) {
+      this.selectedTabItem = event.currentTarget.id
+      this.$router.push(router_link)
+    }
+  }
 }
 </script>
